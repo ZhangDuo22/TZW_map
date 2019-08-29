881cd58da9f0b2d8df3b07ab27cfec02ecdd2670
zhangduo22 <1534114641@qq.com>
2019-08-29 12:40:50 +0800

    第一次更新

--- app/src/main/java/com/rabbit/tzw/Fragments/RemindFragment.java
+++ app/src/main/java/com/rabbit/tzw/Fragments/RemindFragment.java
@@ -6,8 +6,11 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,16 +21,29 @@ import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import com.rabbit.tzw.R;
+import com.rabbit.tzw.myself.mySocketHelper;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
 
 import static android.app.Activity.RESULT_OK;
 
 public class RemindFragment extends Fragment implements View.OnClickListener {
+    //线程池和……
+    private Handler mMainHandler;
+    private ExecutorService mThreadPool;
 
     public static final int TAKE_PHOTO = 1;
     private ImageView picture;
@@ -82,14 +98,29 @@ public class RemindFragment extends Fragment implements View.OnClickListener {
                         Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(imageUri));
                         picture.setImageBitmap(bitmap);
                         //压缩图片
-                        bitmap = compressImage(bitmap,200);
+                        bitmap = compressImage(bitmap,100);
                         //base64转码
                         ByteArrayOutputStream bos = new ByteArrayOutputStream();
                         bitmap.compress(Bitmap.CompressFormat.PNG,100,bos);
                         byte[] bb = bos.toByteArray();
-                        String image = Base64.encodeToString(bb, Base64.NO_WRAP);
-                        System.out.println("这里这里");
-                        System.out.println(image);
+                        final String image = Base64.encodeToString(bb, Base64.NO_WRAP);
+                        request("http://api.tianapi.com/txapi/imglajifenlei/?key=f39cfc89e95e7dfe44a38368c7d74163&img=data:image/jpeg;",image);
+//                        String jsonResult = request(httpUrl);
+//                        System.out.println(jsonResult);
+//                        new Thread(new Runnable(){
+//                            @Override
+//                            public void run() {
+//                                JSONObject jsonObject = new JSONObject();
+//                                try {
+//                                    jsonObject.accumulate("KEY","Login");
+//                                    jsonObject.accumulate("username",image);
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
+//                                mySocketHelper mSocketHelper = new mySocketHelper();
+//                                mSocketHelper.sendDataString(jsonObject);
+//                            }
+//                        }).start();
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     }
@@ -129,4 +160,44 @@ public class RemindFragment extends Fragment implements View.OnClickListener {
         Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);
         return bitmap;
     }
+//    强大的打印功能
+//    public static void i(String tag, String msg) {  //信息太长,分段打印
+//        //因为String的length是字符数量不是字节数量所以为了防止中文字符过多，
+//        //  把4*1024的MAX字节打印长度改为2001字符数
+//        int max_str_length = 2001 - tag.length();
+//        //大于4000时
+//        while (msg.length() > max_str_length) {
+//            Log.i(tag, msg.substring(0, max_str_length));
+//            msg = msg.substring(max_str_length);
+//        }
+//        //剩余部分
+//        Log.i(tag, msg);
+//    }
+
+    public static String request(String httpUrl, String httpArg) {
+        BufferedReader reader = null;
+        String result = null;
+        StringBuffer sbf = new StringBuffer();
+        httpUrl = httpUrl + "?" + httpArg;
+
+        try {
+            URL url = new URL(httpUrl);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setRequestMethod("GET");
+            InputStream is = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String strRead = null;
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+                sbf.append("\r\n");
+            }
+            reader.close();
+            result = sbf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
