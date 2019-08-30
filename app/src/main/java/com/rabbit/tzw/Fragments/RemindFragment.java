429df0a4d94d36082d95819750284a34ae0745e5
zhangduo22 <1534114641@qq.com>
2019-08-30 20:46:08 +0800

    第一次更新

--- app/src/main/java/com/rabbit/tzw/Fragments/RemindFragment.java
+++ app/src/main/java/com/rabbit/tzw/Fragments/RemindFragment.java
@@ -3,11 +3,11 @@ package com.rabbit.tzw.Fragments;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
@@ -16,28 +16,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import com.rabbit.tzw.R;
 import com.rabbit.tzw.myself.mySocketHelper;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.concurrent.ExecutorService;
-
 import static android.app.Activity.RESULT_OK;
 
 public class RemindFragment extends Fragment implements View.OnClickListener {
@@ -48,6 +41,8 @@ public class RemindFragment extends Fragment implements View.OnClickListener {
     public static final int TAKE_PHOTO = 1;
     private ImageView picture;
     private Uri imageUri;
+    private String img;
+    private TextView mTv12;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -85,7 +80,7 @@ public class RemindFragment extends Fragment implements View.OnClickListener {
                 }
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, TAKE_PHOTO);
+                startActivityForResult(intent, TAKE_PHOTO);break;
             }
         }
     }
@@ -96,31 +91,19 @@ public class RemindFragment extends Fragment implements View.OnClickListener {
                 if( resultCode == RESULT_OK ) {
                     try{
                         Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(imageUri));
+                        //旋转图片
+                        bitmap = rotateBitmap(bitmap);
                         picture.setImageBitmap(bitmap);
                         //压缩图片
-                        bitmap = compressImage(bitmap,100);
+                        bitmap = compressImage(bitmap,70);
                         //base64转码
                         ByteArrayOutputStream bos = new ByteArrayOutputStream();
                         bitmap.compress(Bitmap.CompressFormat.PNG,100,bos);
                         byte[] bb = bos.toByteArray();
                         final String image = Base64.encodeToString(bb, Base64.NO_WRAP);
-                        request("http://api.tianapi.com/txapi/imglajifenlei/?key=f39cfc89e95e7dfe44a38368c7d74163&img=data:image/jpeg;",image);
-//                        String jsonResult = request(httpUrl);
-//                        System.out.println(jsonResult);
-//                        new Thread(new Runnable(){
-//                            @Override
-//                            public void run() {
-//                                JSONObject jsonObject = new JSONObject();
-//                                try {
-//                                    jsonObject.accumulate("KEY","Login");
-//                                    jsonObject.accumulate("username",image);
-//                                } catch (JSONException e) {
-//                                    e.printStackTrace();
-//                                }
-//                                mySocketHelper mSocketHelper = new mySocketHelper();
-//                                mSocketHelper.sendDataString(jsonObject);
-//                            }
-//                        }).start();
+                        setimage(image);
+                        MyThread myThread = new MyThread();
+                        myThread.start();
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     }
@@ -133,6 +116,7 @@ public class RemindFragment extends Fragment implements View.OnClickListener {
     private void initData(){
         Button takePhoto = getActivity().findViewById(R.id.remind_photo0);
         picture = getActivity().findViewById(R.id.picture);
+        mTv12 = getActivity().findViewById(R.id.tv_12);
         takePhoto.setOnClickListener(this);
     }
     //压缩图片方法
@@ -143,7 +127,7 @@ public class RemindFragment extends Fragment implements View.OnClickListener {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         // 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
         image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        int options = 50;
+        int options = 110;
         // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
         while (baos.toByteArray().length / 1024 > size) {
             // 重置baos即清空baos
@@ -160,44 +144,72 @@ public class RemindFragment extends Fragment implements View.OnClickListener {
         Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);
         return bitmap;
     }
-//    强大的打印功能
-//    public static void i(String tag, String msg) {  //信息太长,分段打印
-//        //因为String的length是字符数量不是字节数量所以为了防止中文字符过多，
-//        //  把4*1024的MAX字节打印长度改为2001字符数
-//        int max_str_length = 2001 - tag.length();
-//        //大于4000时
-//        while (msg.length() > max_str_length) {
-//            Log.i(tag, msg.substring(0, max_str_length));
-//            msg = msg.substring(max_str_length);
-//        }
-//        //剩余部分
-//        Log.i(tag, msg);
-//    }
-
-    public static String request(String httpUrl, String httpArg) {
-        BufferedReader reader = null;
-        String result = null;
-        StringBuffer sbf = new StringBuffer();
-        httpUrl = httpUrl + "?" + httpArg;
-
-        try {
-            URL url = new URL(httpUrl);
-            HttpURLConnection connection = (HttpURLConnection) url
-                    .openConnection();
-            connection.setRequestMethod("GET");
-            InputStream is = connection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            String strRead = null;
-            while ((strRead = reader.readLine()) != null) {
-                sbf.append(strRead);
-                sbf.append("\r\n");
+    public class MyThread extends Thread{
+        @Override
+        public void run() {
+            String image = getimage();
+            try {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.accumulate("KEY", "Photo");
+                jsonObject.accumulate("image", image.substring(0, 950));
+                jsonObject.accumulate("condition", "first");
+                mySocketHelper mSocketHelper = new mySocketHelper();
+                mSocketHelper.sendDataString(jsonObject);
+                image = image.substring(950);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-            reader.close();
-            result = sbf.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
+            while (true) {
+                int max = 800;
+                try {
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.accumulate("KEY", "Photo");
+                    jsonObject.accumulate("image", image.substring(0,max));
+                    jsonObject.accumulate("condition", "no");
+                    mySocketHelper mSocketHelper = new mySocketHelper();
+                    mSocketHelper.sendDataString(jsonObject);
+                    image = image.substring(max);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if(image.length() - max < 40){
+                    try {
+                        JSONObject jsonObject = new JSONObject();
+                        jsonObject.accumulate("KEY", "Photo");
+                        jsonObject.accumulate("image", image);
+                        jsonObject.accumulate("condition", "yes");
+                        mySocketHelper mSocketHelper = new mySocketHelper();
+                        mSocketHelper.sendDataString(jsonObject);
+                        String sRv = mSocketHelper.getDataString();
+                        Log.i("张夺来巡山了",sRv);
+                        mTv12.setText(sRv);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
 
+        }
+    }
+    private void setimage(String img){
+        this.img = img;
+    }
+    private String getimage(){
+        return img;
+    }
+     //旋转图片
+     public static Bitmap rotateBitmap(Bitmap bitmap) {
+         int width = bitmap.getWidth();
+         int height = bitmap.getHeight();
+
+         if(width < height) {
+             bitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getWidth());
+         }else {
+             Matrix matrix = new Matrix();
+             matrix.postRotate(90);
+             bitmap = Bitmap.createBitmap(bitmap,0,0,bitmap.getHeight(),bitmap.getHeight(),matrix,true);
+         }
+         return bitmap;
+    }
 }
